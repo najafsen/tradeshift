843be978c542e1ef327e44f58b5ee5977f29665e
Mohsen Najaflou <mohsen.najaflou@gmail.com>
2018-11-20 00:42:49 +0330

    test (Panel): add tests

---
+++ src/components/Panel.test.tsx
@@ -0,0 +1,23 @@
+import {shallow} from 'enzyme';
+import React from 'react';
+import Panel from './Panel';
+
+describe('Panel', () => {
+    it('Should render a simple ts panel correctly', () => {
+        const panel = shallow(<Panel/>);
+
+        expect(panel.html()).toBe('<div data-ts="Panel" data-ts.label=""></div>');
+    });
+
+    it('Should render a simple ts panel with some div props correctly', () => {
+        const panel = shallow(<Panel className="ts" title="tradeshift"/>);
+
+        expect(panel.html()).toBe('<div data-ts="Panel" data-ts.label="" class="ts" title="tradeshift"></div>');
+    });
+
+    it('Should render a simple ts panel with label and some div props correctly', () => {
+        const panel = shallow(<Panel label="tradeshift" className="ts" title="tradeshift"/>);
+
+        expect(panel.html()).toBe('<div data-ts="Panel" data-ts.label="tradeshift" class="ts" title="tradeshift"></div>');
+    });
+});
