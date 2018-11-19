6cfbc159989a83e279e8b80ffed6597c2e08e2e4
Mohsen Najaflou <mohsen.najaflou@gmail.com>
2018-11-20 00:37:52 +0330

    test (MainContent): add tests

---
+++ src/components/MainContent.test.tsx
@@ -0,0 +1,17 @@
+import {shallow} from 'enzyme';
+import React from 'react';
+import MainContent from './MainContent';
+
+describe('MainContent', () => {
+    it('Should render a simple ts mainContent correctly', () => {
+        const mainContent = shallow(<MainContent/>);
+
+        expect(mainContent.html()).toBe('<div data-ts="MainContent"></div>');
+    });
+
+    it('Should render a simple ts mainContent with some div props correctly', () => {
+        const mainContent = shallow(<MainContent className="ts" title="tradeshift"/>);
+
+        expect(mainContent.html()).toBe('<div data-ts="MainContent" class="ts" title="tradeshift"></div>');
+    });
+});
